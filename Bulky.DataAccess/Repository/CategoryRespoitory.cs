dca74a6528d8251482e38456accb2cda30d9bef5
iman <dev.imansabet@gmail.com>
2023-11-13 16:36:36 +0330

    Repository Interface  was  Added + Implementaion done Also for Category

---
+++ Bulky.DataAccess/Repository/CategoryRespoitory.cs
@@ -0,0 +1,32 @@
+﻿using Bulky.DataAccess.Data;
+using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulky.DataAccess.Repository
+{
+    public class CategoryRespoitory : Repository<Category> , ICategoryRepository
+    {
+        private ApplicationDbContext _db;
+        public CategoryRespoitory(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+
+        public void Update(Category obj)
+        {
+            _db.Categories.Update(obj);
+        }
+    }
+}
