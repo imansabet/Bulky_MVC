2f85a5099f6221b2ee23778f764ca0b41acf1a15
iman <dev.imansabet@gmail.com>
2023-11-17 13:55:52 +0330

    Combine Edit & Create pages (Upsert method)

--- BulkyWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyWeb/Areas/Admin/Controllers/ProductController.cs
@@ -23,7 +23,7 @@ namespace BulkyWeb.Areas.Admin.Controllers
             return View(objProductList);
         }
 
-        public IActionResult Create()
+        public IActionResult Upsert(int? id)
         {
             ProductVM productVM = new()
             {
@@ -36,12 +36,22 @@ namespace BulkyWeb.Areas.Admin.Controllers
                 ),
                 Product = new Product()
             };
-            return View(productVM);
+            if(id == null || id == 0 )
+            {
+                //cretae
+                return View(productVM);
+            }else
+            {
+                //update
+                productVM.Product = _unitOfWork.Product.Get(u => u.Id == id );
+                return View(productVM);
+
+            }
         }
 
 
         [HttpPost]
-        public IActionResult Create(ProductVM productVM)
+        public IActionResult Upsert(ProductVM productVM , IFormFile ? file)
         {
             
             if (ModelState.IsValid)
@@ -63,31 +73,7 @@ namespace BulkyWeb.Areas.Admin.Controllers
             return View(productVM);
             }
         }
-        public IActionResult Edit(int? id)
-        {
-            if (id == null || id == 0)
-            {
-                return NotFound();
-            }
-            Product? ProductFromDb = _unitOfWork.Product.Get(u => u.Id == id);
-            if (ProductFromDb == null)
-            {
-                return NotFound();
-            }
-            return View(ProductFromDb);
-        }
-        [HttpPost]
-        public IActionResult Edit(Product obj)
-        {
-            if (ModelState.IsValid)
-            {
-                _unitOfWork.Product.Update(obj);
-                _unitOfWork.Save();
-                TempData["success"] = "Product updated successfully";
-                return RedirectToAction("Index");
-            }
-            return View();
-        }
+        
         public IActionResult Delete(int? id)
         {
             if (id == null || id == 0)
@@ -101,6 +87,10 @@ namespace BulkyWeb.Areas.Admin.Controllers
             }
             return View(ProductFromDb);
         }
+
+
+
+
         [HttpPost, ActionName("Delete")]
         public IActionResult DeletePOST(int? id)
         {
